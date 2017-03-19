d6a7407d7cafb63320cf123794d69aeba48a7dc9
pankhiprasher <pprasher@asu.edu>
2017-03-19 15:44:53 -0700

    project page

--- src/edu/asuse/model/Project.java
+++ src/edu/asuse/model/Project.java
@@ -1,21 +1,35 @@
 package edu.asuse.model;
 
+import java.sql.Timestamp;
+
 public class Project {
 	String name;
 	String description;
 	String created_by;
 	String use_case_template;
 	String policy_name;
-	int dev_mgr_duration;
-	int soln_mgr_duration;
-	int arch_duration;
-	int qa_duration;
+	String status;
+	Timestamp ts ;
+	
+	
 	
 	public String getName() {
 		return name;
 	}
 	public void setName(String name) {
 		this.name = name;
+	}
+	public Timestamp getTs() {
+		return ts;
+	}
+	public void setTs(Timestamp ts) {
+		this.ts = ts;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
 	}
 	public String getDescription() {
 		return description;
@@ -41,35 +55,14 @@ public class Project {
 	public void setPolicy_name(String policy_name) {
 		this.policy_name = policy_name;
 	}
-	public int getDev_mgr_duration() {
-		return dev_mgr_duration;
-	}
-	public void setDev_mgr_duration(int dev_mgr_duration) {
-		this.dev_mgr_duration = dev_mgr_duration;
-	}
-	public int getSoln_mgr_duration() {
-		return soln_mgr_duration;
-	}
-	public void setSoln_mgr_duration(int soln_mgr_duration) {
-		this.soln_mgr_duration = soln_mgr_duration;
-	}
-	public int getArch_duration() {
-		return arch_duration;
-	}
-	public void setArch_duration(int arch_duration) {
-		this.arch_duration = arch_duration;
-	}
-	public int getQa_duration() {
-		return qa_duration;
-	}
-	public void setQa_duration(int qa_duration) {
-		this.qa_duration = qa_duration;
-	}
+	
 	@Override
 	public String toString(){
 		String res = "Name: " + this.getName() +
 				"\nDescription: " + this.getDescription() +
 				"\nCreated by: " + this.getCreated_by() +
+				"\nStatus: " + this.getStatus() +
+				"\nCreated time: " + this.getTs() +
 				"\nUse Case Template: " + this.getUse_case_template() +
 				"\nPolicy name: " + this.getPolicy_name();
 		return res;

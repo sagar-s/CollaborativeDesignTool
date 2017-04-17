f4d79e5181330c569f42fc5127ba894880f2118b
pankhiprasher <pprasher@asu.edu>
2017-04-16 19:49:56 -0700

    Email service interface

--- src/edu/asuse/dao/EmailService.java
+++ src/edu/asuse/dao/EmailService.java
@@ -2,8 +2,5 @@ package edu.asuse.dao;
 import org.springframework.mail.SimpleMailMessage;
 
 public interface EmailService {
-	
-	public void sendEmail(SimpleMailMessage message);
-		
-	
+	public boolean sendEmail(SimpleMailMessage message); 
 }
